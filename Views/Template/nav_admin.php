bd2467ee5343f00955b5f1aa9bacbdfbbd28e826
Rene Alejandro Vasquez <reneravv1@gmail.com>
2025-11-27 17:39:20 -0400

    feat (Nav): Actualización de acceso por permisos de plantilla-menú NAV

--- Views/Template/nav_admin.php
+++ Views/Template/nav_admin.php
@@ -10,11 +10,8 @@
   </div>
   <ul class="app-menu">
 
-    <!-- PERMISOS DASHBOARD -->
-    <?php 
-    // Verifica el permiso de Lectura (r) para el Módulo 1 (Dashboard)
-    if(!empty($_SESSION['permisos'][1]['r'])){ 
-    ?>
+    <!-- DASHBOARD -->
+    <?php if(!empty($_SESSION['permisos'][1]['r'])){ ?>
     <li>
       <a class="app-menu__item" href="<?= base_url(); ?>/dashboard">
         <i class="app-menu__icon fa fa-dashboard"></i>
@@ -23,10 +20,8 @@
     </li>
     <?php } ?>
 
-    <?php 
-    // Verifica si tiene permiso de Lectura para el Módulo 2 (Usuarios) O Módulo 3 (Roles)
-    if(!empty($_SESSION['permisos'][2]['r']) || !empty($_SESSION['permisos'][3]['r'])){ 
-    ?>
+    <!-- GESTIÓN DE SEGURIDAD -->
+    <?php if(!empty($_SESSION['permisos'][2]['r']) || !empty($_SESSION['permisos'][3]['r'])){ ?>
     <li class="treeview">
       <a class="app-menu__item" href="#" data-toggle="treeview">
         <i class="app-menu__icon fa fa-users" aria-hidden="true"></i>
@@ -44,80 +39,151 @@
     </li>
     <?php } ?>
     
-    <?php 
-    // Verifica el permiso de Lectura (r) para el Módulo 4 (Departamentos)
-    if(!empty($_SESSION['permisos'][4]['r'])){ 
-    ?>
-    <li>
-      <a class="app-menu__item" href="<?= base_url(); ?>/departamentos">
-        <i class="app-menu__icon fa fa-building" aria-hidden="true"></i>
-        <span class="app-menu__label">Departamentos</span>
+    <!-- RECURSOS HUMANOS -->
+    <?php if(!empty($_SESSION['permisos'][11]['r']) || !empty($_SESSION['permisos'][10]['r'])){ ?>
+    <li class="treeview">
+      <a class="app-menu__item" href="#" data-toggle="treeview">
+        <i class="app-menu__icon fas fa-users-cog"></i>
+        <span class="app-menu__label">Recursos Humanos</span>
+        <i class="treeview-indicator fa fa-angle-right"></i>
       </a>
-    </li>
-    <?php } ?>
-    
-    <?php if (!empty($_SESSION['permisos'][5]['r'])): ?>
-
-        <li class="treeview">
-            <a class="app-menu__item" href="#" data-toggle="treeview">
-                <i class="app-menu__icon fas fa-users-cog"></i> 
-                <span class="app-menu__label">Recursos Humanos</span>
-                <i class="treeview-indicator fa fa-angle-right"></i>
-            </a>
-            <ul class="treeview-menu">
-                
-                <li>
-                    <a class="treeview-item" href="<?= base_url(); ?>/Trabajador">
-                        <i class="icon fas fa-id-badge"></i>
-                        Trabajadores
-                    </a>
-                </li>
-                
-                </ul>
+      <ul class="treeview-menu">
+        <?php if(!empty($_SESSION['permisos'][11]['r'])){ ?>
+        <li>
+          <a class="treeview-item" href="<?= base_url(); ?>/trabajador">
+            <i class="icon fas fa-id-badge"></i>
+            Trabajadores
+          </a>
         </li>
-
-    <?php endif; ?>
-
-    <?php 
-    // Verifica el permiso de Lectura (r) para el Módulo 5 (Tareas)
-    if(!empty($_SESSION['permisos'][12]['r'])){ 
-    ?>
-    <li>
-      <a class="app-menu__item" href="<?= base_url(); ?>/tareas">
-        <i class="app-menu__icon fa fa-tasks" aria-hidden="true"></i>
-        <span class="app-menu__label">Tareas y Carga Laboral</span>
-      </a>
+        <?php } ?>
+        <?php if(!empty($_SESSION['permisos'][10]['r'])){ ?>
+        <li>
+          <a class="treeview-item" href="<?= base_url(); ?>/departamentos">
+            <i class="icon fa fa-building"></i>
+            Departamentos
+          </a>
+        </li>
+        <?php } ?>
+      </ul>
     </li>
     <?php } ?>
 
-    <?php 
-    // Verifica el permiso de Lectura (r) para el Módulo 6 (Indicadores)
-    if(!empty($_SESSION['permisos'][13]['r'])){ 
-    ?>
-    <li>
-      <a class="app-menu__item" href="<?= base_url(); ?>/indicadores">
-        <i class="app-menu__icon fa fa-chart-line" aria-hidden="true"></i>
-        <span class="app-menu__label">Indicadores de Estrés</span>
+    <!-- GESTIÓN DE ENCUESTAS -->
+    <?php if(!empty($_SESSION['permisos'][20]['r']) || !empty($_SESSION['permisos'][21]['r']) || !empty($_SESSION['permisos'][22]['r'])){ ?>
+    <li class="treeview">
+      <a class="app-menu__item" href="#" data-toggle="treeview">
+        <i class="app-menu__icon fa fa-clipboard-list"></i>
+        <span class="app-menu__label">Gestión de Encuestas</span>
+        <i class="treeview-indicator fa fa-angle-right"></i>
       </a>
+      <ul class="treeview-menu">
+        <?php if(!empty($_SESSION['permisos'][20]['r'])){ ?>
+        <li>
+          <a class="treeview-item" href="<?= base_url(); ?>/encuestas">
+            <i class="icon fa fa-poll"></i>
+            Encuestas de Estrés
+          </a>
+        </li>
+        <?php } ?>
+        <?php if(!empty($_SESSION['permisos'][21]['r'])){ ?>
+        <li>
+          <a class="treeview-item" href="<?= base_url(); ?>/preguntas">
+            <i class="icon fa fa-question-circle"></i>
+            Banco de Preguntas
+          </a>
+        </li>
+        <?php } ?>
+        <?php if(!empty($_SESSION['permisos'][22]['r'])){ ?>
+        <li>
+          <a class="treeview-item" href="<?= base_url(); ?>/categorias">
+            <i class="icon fa fa-tags"></i>
+            Categorías de Indicadores
+          </a>
+        </li>
+        <?php } ?>
+      </ul>
     </li>
     <?php } ?>
-    
-    <?php 
-    // Verifica el permiso de Lectura (r) para el Módulo 7 (Bitácora Emocional)
-    if(!empty($_SESSION['permisos'][14]['r'])){ 
-    ?>
-    <li>
-      <a class="app-menu__item" href="<?= base_url(); ?>/bitacora">
-        <i class="app-menu__icon fa fa-book" aria-hidden="true"></i>
-        <span class="app-menu__label">Bitácora Emocional</span>
+
+    <!-- GESTIÓN OPERATIVA -->
+    <?php if(!empty($_SESSION['permisos'][30]['r']) || !empty($_SESSION['permisos'][31]['r']) || !empty($_SESSION['permisos'][32]['r'])){ ?>
+    <li class="treeview">
+      <a class="app-menu__item" href="#" data-toggle="treeview">
+        <i class="app-menu__icon fa fa-tasks"></i>
+        <span class="app-menu__label">Gestión Operativa</span>
+        <i class="treeview-indicator fa fa-angle-right"></i>
       </a>
+      <ul class="treeview-menu">
+        <?php if(!empty($_SESSION['permisos'][30]['r'])){ ?>
+        <li>
+          <a class="treeview-item" href="<?= base_url(); ?>/tareas">
+            <i class="icon fa fa-tasks"></i>
+            Tareas y Carga Laboral
+          </a>
+        </li>
+        <?php } ?>
+        <?php if(!empty($_SESSION['permisos'][31]['r'])){ ?>
+        <li>
+          <a class="treeview-item" href="<?= base_url(); ?>/tareas/mistareas">
+            <i class="icon fa fa-list-alt"></i>
+            Mis Tareas Asignadas
+          </a>
+        </li>
+        <?php } ?>
+        <?php if(!empty($_SESSION['permisos'][32]['r'])){ ?>
+        <li>
+          <a class="treeview-item" href="<?= base_url(); ?>/bitacora">
+            <i class="icon fa fa-book"></i>
+            Bitácora Emocional
+          </a>
+        </li>
+        <?php } ?>
+      </ul>
     </li>
     <?php } ?>
-    
+
+    <!-- ANÁLISIS Y MONITOREO -->
+    <?php if(!empty($_SESSION['permisos'][40]['r']) || !empty($_SESSION['permisos'][41]['r']) || !empty($_SESSION['permisos'][42]['r'])){ ?>
+    <li class="treeview">
+      <a class="app-menu__item" href="#" data-toggle="treeview">
+        <i class="app-menu__icon fa fa-chart-line"></i>
+        <span class="app-menu__label">Análisis y Monitoreo</span>
+        <i class="treeview-indicator fa fa-angle-right"></i>
+      </a>
+      <ul class="treeview-menu">
+        <?php if(!empty($_SESSION['permisos'][40]['r'])){ ?>
+        <li>
+          <a class="treeview-item" href="<?= base_url(); ?>/indicadores">
+            <i class="icon fa fa-heartbeat"></i>
+            Indicadores de Estrés
+          </a>
+        </li>
+        <?php } ?>
+        <?php if(!empty($_SESSION['permisos'][41]['r'])){ ?>
+        <li>
+          <a class="treeview-item" href="<?= base_url(); ?>/intervenciones">
+            <i class="icon fa fa-life-ring"></i>
+            Intervenciones
+          </a>
+        </li>
+        <?php } ?>
+        <?php if(!empty($_SESSION['permisos'][42]['r'])){ ?>
+        <li>
+          <a class="treeview-item" href="<?= base_url(); ?>/analiticas">
+            <i class="icon fa fa-chart-bar"></i>
+            Analíticas y Reportes
+          </a>
+        </li>
+        <?php } ?>
+      </ul>
+    </li>
+    <?php } ?>
+
+    <!-- LOGOUT -->
     <li>
       <a class="app-menu__item" href="<?= base_url(); ?>/logout">
         <i class="app-menu__icon fa fa-sign-out" aria-hidden="true"></i>
-        <span class="app-menu__label">Logout</span>
+        <span class="app-menu__label">Cerrar Sesión</span>
       </a>
     </li>
   </ul>
